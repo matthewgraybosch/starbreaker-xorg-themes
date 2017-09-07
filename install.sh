d9aa06f1ad58ef8d80e8698528da78c3b3a57613
Matthew Graybosch <public@matthewgraybosch.com>
2017-09-07 01:14:18 -0400

    Added basic install/uninstall scripts

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.themes
+cp -R Biomechanical/ ~/.themes/
+cp -R Dark\ Eternal/ ~/.themes/
+cp -R Energistic/ ~/.themes/
+cp -R Foxfire/ ~/.themes/
+cp -R Infrared/ ~/.themes/
+cp -R Preternatural/ ~/.themes/
+cp -R Visible\ Spectrum/ ~/.themes/
---
+++ uninstall.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rm -rf ~/.themes/Biomechanical/
+rm -rf ~/.themes/Dark\ Eternal/
+rm -rf ~/.themes/Energistic/
+rm -rf ~/.themes/Foxfire/
+rm -rf ~/.themes/Infrared/
+rm -rf ~/.themes/Preternatural/
+rm -rf ~/.themes/Visible\ Spectrum/
